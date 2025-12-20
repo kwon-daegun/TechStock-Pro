3aaf737e9c79aa2d7be5c2a3e8c9ee0c66d0d037
kwon daegun <kalki127001@gmail.com>
2025-12-20 19:17:26 +0530

    add session to store who is currently logged into the app

---
+++ src/InventorySystem/InventorySystem/Services/Session.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventorySystem.Core.Models;
+
+namespace InventorySystem.Services {
+    public static class Session {
+        public static User? CurrentUser { get; private set; }
+
+        public static void Start(User user) {
+            CurrentUser = user;
+        }
+
+        public static void Stop() {
+            CurrentUser = null;
+        }
+
+        public static bool IsAdmin() {
+            return CurrentUser?.Role == "Admin";
+        }
+    }
+}
