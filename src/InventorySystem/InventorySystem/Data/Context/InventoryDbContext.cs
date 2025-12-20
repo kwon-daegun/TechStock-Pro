09eab451118032156d59dec36b983a9f18068333
kwon daegun <kalki127001@gmail.com>
2025-12-20 19:12:28 +0530

    add login page database

---
+++ src/InventorySystem/InventorySystem/Data/Context/InventoryDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using InventorySystem.Core.Models;
+
+namespace InventorySystem.Data {
+    public class InventoryDbContext : DbContext {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TechStockDb;Trusted_Connection=True;");
+        }
+    }
+}
