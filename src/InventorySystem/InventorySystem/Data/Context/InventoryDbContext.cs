d648d9666b8431970a489d27c4e0891d66354b90
kwon daegun <kalki127001@gmail.com>
2025-12-23 04:34:47 +0530

    add Supplier context

--- src/InventorySystem/InventorySystem/Data/Context/InventoryDbContext.cs
+++ src/InventorySystem/InventorySystem/Data/Context/InventoryDbContext.cs
@@ -10,6 +10,7 @@ namespace InventorySystem.Data {
     public class InventoryDbContext : DbContext {
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
