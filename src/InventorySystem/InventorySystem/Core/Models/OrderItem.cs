8bcd8530a00a4c819a263b23db0c115e3b4294d9
kwon daegun <kalki127001@gmail.com>
2025-12-22 12:47:47 +0530

    add order item details like price quantity etc

---
+++ src/InventorySystem/InventorySystem/Core/Models/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InventorySystem.Core.Models {
+    public class OrderItem {
+        [Key]
+        public int Id { get; set; }
+
+        // Link to the Order
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public virtual Order Order { get; set; } = null!;
+
+        // Link to the Product
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        // Storing price here too, in case the product price changes later.
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Schema.NotMapped]
+        public decimal Total => UnitPrice * Quantity;
+    }
+}
